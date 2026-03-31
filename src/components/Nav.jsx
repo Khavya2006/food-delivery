4d7536f81ba0958368f99ce5ad78127c7bd10ee6
Khavya2006 <khavya2006@gmail.com>
2026-03-31 14:41:56 +0530

    added new files

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <div className='width-full h-[100px] bg-slate-400'>
+        </div>
+
+
+    )
+
+}
+export default Nav;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../components/Nav";
+function Home(){
+    return(
+        <div className='bg-slate-800 w-full min-h-screen'>
+       <Nav/>
+
+        </div>
+
+
+    )
+
+}
+export default Home;
+
