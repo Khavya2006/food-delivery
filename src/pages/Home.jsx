caf0a26509d2759857b505fc36426e744b4421be
Khavya2006 <khavya2006@gmail.com>
2026-04-03 12:44:48 +0530

    added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "../components/Nav";
 function Home(){
     return(
-        <div className='bg-slate-800 w-full min-h-screen'>
+        <div className='bg-slate-200 w-full min-h-screen'>
        <Nav/>
 
         </div>
