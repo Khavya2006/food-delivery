a1dbdaf99ab4fa9b19122f94c4875d5aed129c41
Khavya2006 <khavya2006@gmail.com>
2026-04-03 14:39:19 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
-
+import Card from "./components/Card";
 function App() {
   return (
     <div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Categories from "../Category";
 function Home(){
     return(
         <div className='bg-slate-200 w-full min-h-screen'>
        <Nav/>
+       <div className="flex flex-wrap justify-center items-center gap-6 w-[100%] text-center">
+        {Categories.map((item)=>{
+        return<div className="w-[140px] h-[150] bg-white flex flex-col items-start gap-5 p-5 justify-start text-[20px] font-semibold text-gray-600 rounded-lg shawdoe-xl hover:bg-green-200 cursor-pointer transition-all duration-200">
+               {item.icon}
+                {item.name}
+            </div>
+
+})}
+       </div>
 
         </div>
 
